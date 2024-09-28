303160e29350acba8356fada290f6b3d7594578d
Rajan Patel <rkp1505.l@gmail.com>
2024-09-28 15:32:28 +0530

    appjs start

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express= require('express');
-c
+const app=express();
+
+const cookieParser = require("cookie-parser");
+const path= require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookie-parser());
+app.use(express.static(path.join(__dirname,"public")));
+app.set("/", (req,res)=>{
+   res.send("Hello");
+
+});
+
+app.listen(3000);
